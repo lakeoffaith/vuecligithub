641e03871dc7a01b57e30094333e8826636d5ff6
whiteubuntu <905663280@qq.com>
2020-08-08 10:35:19 +0800

    first commit

--- README.md
+++ README.md
@@ -22,3 +22,8 @@ npm run lint
 
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
+
+
+## project部署到github page
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld msg="Vue 项目部署到github page"/>
   </div>
 </template>
 
